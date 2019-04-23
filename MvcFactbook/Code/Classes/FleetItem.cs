9ff8033029712adb72af0e2909180c2683254ae3
Andy Smith <asmith@ScottLogic.com>
2019-04-23 15:28:34 +0100

    Start new Fleet class

---
+++ MvcFactbook/Code/Classes/FleetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcFactbook.Code.Classes
+{
+    public class FleetItem
+    {
+
+
+
+
+
+    }
+}
