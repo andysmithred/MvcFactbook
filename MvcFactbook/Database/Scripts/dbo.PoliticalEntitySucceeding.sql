5367d286935dfeb1bff815c3710eca23612d67e4
Andy Smith <asmith@ScottLogic.com>
2019-04-23 17:01:04 +0100

    Database commit

---
+++ MvcFactbook/Database/Scripts/dbo.PoliticalEntitySucceeding.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PoliticalEntitySucceeding] (
+    [Id]                          INT IDENTITY (1, 1) NOT NULL,
+    [PoliticalEntityId]           INT NOT NULL,
+    [SucceedingPoliticalEntityId] INT NOT NULL,
+    [Year] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_PoliticalEntitySucceeding_PoliticalEntityId_To_PoliticalEntity] FOREIGN KEY ([PoliticalEntityId]) REFERENCES [dbo].[PoliticalEntity] ([Id]),
+    CONSTRAINT [FK_PoliticalEntitySucceeding_SucceedingPoliticalEntityId_To_PoliticalEntity] FOREIGN KEY ([SucceedingPoliticalEntityId]) REFERENCES [dbo].[PoliticalEntity] ([Id])
+);
+
