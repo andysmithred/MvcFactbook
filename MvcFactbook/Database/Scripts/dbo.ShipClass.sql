7f6e6efb257dba9feb43d0905d37cae87e38b293
Andy Smith <asmith@ScottLogic.com>
2019-05-10 16:57:39 +0100

    Database commit

---
+++ MvcFactbook/Database/Scripts/dbo.ShipClass.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ShipClass] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX) NOT NULL,
+    [SubClassName] NVARCHAR (MAX) NULL,
+    [Displacement] INT            NULL,
+    [Length]       FLOAT (53)     NULL,
+    [Beam]         FLOAT (53)     NULL,
+    [Propulsion]   INT            NULL,
+    [Speed]        FLOAT (53)     NULL,
+    [Crew]         INT            NULL,
+    [Year] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
