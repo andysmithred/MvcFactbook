7a4332c74ce3c435772d9983c130b55c6c911938
Andy Smith <andysmithred@yahoo.com>
2019-09-11 22:55:47 +0100

    db commit

--- MvcFactbook/Database/Scripts/dbo.Builder.sql
+++ MvcFactbook/Database/Scripts/dbo.Builder.sql
@@ -1,9 +1,13 @@
 ﻿CREATE TABLE [dbo].[Builder] (
-    [Id]      INT            IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (MAX) NOT NULL,
-    [Founded] INT            NULL,
-    [Defunct] INT            NULL,
-    [Complete] BIT NOT NULL DEFAULT 0, 
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Name]            NVARCHAR (MAX) NOT NULL,
+    [Founded]         INT            NULL,
+    [Defunct]         INT            NULL,
+    [Complete]        BIT            DEFAULT ((0)) NOT NULL,
+    [ShortName]       NVARCHAR (50)  NULL,
+    [AlternativeNames] NVARCHAR (MAX) NULL,
+    [Location] NVARCHAR(MAX) NULL, 
+    [Parent] NVARCHAR(MAX) NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
