56d71183d8b1f42e0738d0ce1b4d80a5eb97c998
Andy Smith <asmith@ScottLogic.com>
2019-10-01 17:59:49 +0100

    db commit

---
+++ MvcFactbook/Database/Scripts/dbo.DockyardHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DockyardHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [DockyardId] INT NOT NULL, 
+    [ShipbuilderId] INT NOT NULL, 
+    [Start] INT NULL, 
+    [End] INT NULL,
+	CONSTRAINT [FK_DockyardHistory_DockyardId_To_Dockyard] FOREIGN KEY ([DockyardId]) REFERENCES [dbo].[Dockyard] ([Id]),
+    CONSTRAINT [FK_DockyardHistory_ShipbuilderId_To_Shipbuilder] FOREIGN KEY ([ShipbuilderId]) REFERENCES [dbo].[Shipbuilder] ([Id])
+)
